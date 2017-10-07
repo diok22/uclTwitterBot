61b7adaaafde5c0800fe250c5fa8ef2736709fa6
keelanfh <info@keelanfh.com>
2017-10-07 15:09:44 +0100

    fix bug with bookings.json

--- make_string.py
+++ make_string.py
@@ -1,23 +1,23 @@
 import json
 from datetime import datetime
 
-with open("bookings.json", 'r') as f:
-    bookings = json.load(f)
-
-tweets = []
-
 
 def tweet_text(booking):
     society = (booking["contact"].split("- ")[1])
     start = datetime.strptime(booking["start_time"][:-6], "%Y-%m-%dT%H:%M:%S")
     return "{} just booked {} for {}".format(society, booking['roomname'], start.strftime("%A %d %B at %H:%M"))
 
-for booking in bookings["bookings"]:
-    tweets.append(tweet_text(booking))
 
+if __name__ == "__main__":
+    with open("bookings.json", 'r') as f:
+        bookings = json.load(f)
 
+    tweets = []
 
-# print(tweets)
-print(len(tweets))
-with open("tweets.json", "w") as f:
-    json.dump(tweets[:5], f)
+    for booking in bookings["bookings"]:
+        tweets.append(tweet_text(booking))
+
+    # print(tweets)
+    print(len(tweets))
+    with open("tweets.json", "w") as f:
+        json.dump(tweets[:5], f)
