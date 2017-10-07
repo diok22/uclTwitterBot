8998786e55d30c42014a0364034943342e42a844
diok22 <Dionysis.k22@gmail.com>
2017-10-07 12:29:21 +0100

    twitter api keys, will hide later

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+var TwitterPackage = require('twitter');
+
+var secret = {
+  consumer_key: 'nyEntYwjp4NGk27KahzEa4vE3',
+  consumer_secret: 'GabiomPL6SFbJTeprOVn5LYp4x8KhAn8eXaf81I7vRAS8DrhOL
+',
+  access_token_key: '916621288604618757-gD09oNVprueTt8iD2QtJNVRIpDAOiZi',
+  access_token_secret: '08UyMBfUbDgy30R49ltsk5eTBnyzx3ExbfppCfIO3d0Ql'
+}
+var Twitter = new TwitterPackage(secret);
