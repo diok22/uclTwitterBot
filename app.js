616a817a403d964245df1b18f20dd3ef5f6a2d5c
diok22 <Dionysis.k22@gmail.com>
2017-10-07 16:33:55 +0100

    bookinglisterner model

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var TwitterPackage = require('twitter');
-
+var BookingListener = require('./models/SocietyBookingListener.js');
 var secret = require("./secret")
 
 var Twitter = new TwitterPackage(secret)
 
-// var json = require('./tweets.json')
-// console.log(json.length)
-// var jsonFirst = json[0]
+var json = require('./tweets.json')
+console.log(json.length)
+var jsonFirst = json[0]
+console.log(jsonFirst);
 
 
 Twitter.stream('statuses/filter', {track: '@UCLSocBot'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
+    if (tweet.text.includes("#societybookings")) {
+
   });
 
   stream.on('error', function(error) {
---
+++ models/SocietyBookingListener.js
@@ -0,0 +1,5 @@
+var exports = module.exports = {};
+
+exports.bookingListener = function(tweet) {
+  
+};
