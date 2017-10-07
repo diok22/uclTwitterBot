bb07fcf13c504251c2b56384061eaf47490316ec
keelanfh <info@keelanfh.com>
2017-10-07 13:32:04 +0100

    change to make_string.py

--- make_string.py
+++ make_string.py
@@ -11,7 +11,6 @@ for booking in bookings["bookings"]:
     start = datetime.strptime(booking["start_time"][:-6], "%Y-%m-%dT%H:%M:%S")
     tweets.append("{} just booked {} for {}".format(society, booking['roomname'], start.strftime("%A %d %B at %H:%M")))
 
-
 # print(tweets)
 print(len(tweets))
 with open("tweets.json", "w") as f:
