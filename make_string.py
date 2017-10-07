0ad25d423d8e14880ac10c68cc072a2babb658fe
keelanfh <info@keelanfh.com>
2017-10-07 13:09:23 +0100

    Make strings in Python

--- make_string.py
+++ make_string.py
@@ -1,12 +1,18 @@
 import json
-import datetime
-import ciso8601
+from datetime import datetime
 
 with open("bookings.json", 'r') as f:
     bookings = json.load(f)
 
+tweets = []
+
 for booking in bookings["bookings"]:
     society = (booking["contact"].split("- ")[1])
-    start = ciso8601.parse_datetime(booking["start_time"])
+    start = datetime.strptime(booking["start_time"][:-6], "%Y-%m-%dT%H:%M:%S")
+    tweets.append("{} just booked {} for {}".format(society, booking['roomname'], start.strftime("%A %d %B at %H:%M")))
 
-    print("{} just booked {} on {}".format(society, booking['roomname'], start.day))
+
+# print(tweets)
+print(len(tweets))
+with open("tweets.json", "w") as f:
+    json.dump(tweets, f)
