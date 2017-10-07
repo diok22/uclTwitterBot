447aac56e17bc8005055093b99410074cc6f77d9
diok22 <Dionysis.k22@gmail.com>
2017-10-07 15:43:42 +0100

    listens to its username

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var secret = require("./secret")
 
 var Twitter = new TwitterPackage(secret)
 
-var json = require('./tweets.json')
-console.log(json.length)
+// var json = require('./tweets.json')
+// console.log(json.length)
 // var jsonFirst = json[0]
 
-var arrayLength = json.length;
-for (var i = 0; i < arrayLength; i++) {
-  Twitter.post('statuses/update', {status: json[i]},  function(error, tweet, response){
-    if(error){
-      console.log(error);
-    }
-    console.log(tweet);  // Tweet body.
-    console.log(response);  // Raw response object.
+
+Twitter.stream('statuses/filter', {track: '@UCLSocBot'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
   });
-}
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
